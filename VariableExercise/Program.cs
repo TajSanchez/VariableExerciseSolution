8283380b76029ff225ab3dcc276c48510596fab9
Taj <tajmarie92@gmail.com>
2022-04-04 18:45:14 -0500

    Add project files.

---
+++ VariableExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            bool isAlive;
+
+            age = 29;
+            name = "Taj";
+            isAlive = true;
+
+            Console.WriteLine($"Hello {name}! You are {age} and you are alive;{isAlive}");
+        }
+    }
+}
